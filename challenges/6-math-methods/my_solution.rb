f3a89ab68afb52ea58af6449655a3b056d47c4cd
javier.vargas <Obloctowarl1>
2017-02-09 19:26:46 -0500

    resolving-challenge-6

--- challenges/6-math-methods/my_solution.rb
+++ challenges/6-math-methods/my_solution.rb
@@ -2,5 +2,19 @@
 
 # Tu solucion abajo:
 
+def add(arg1, arg2)
+	arg1 + arg2
+end
 
+def subtract(arg1, arg2)
+	arg1 - arg2
+end
+
+def multiply(arg1, arg2)
+	arg1 * arg2
+end
+
+def divide(arg1, arg2)
+	arg1 / arg2
+end
 
